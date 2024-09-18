40ccba401362af95b79c954cae6ed0ec1aeb01e4
edthuman <edwardadirvine@aol.com>
2024-09-18 15:48:13 +0100

    add app title to index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit index file to edit this screen.</Text>
+
+  return <View style={styles.page}>
+    <View style={styles.view} >
+      <Text style={styles.text}>SESH</Text>
     </View>
-  );
+  </View>
 }
+
+const styles = StyleSheet.create({
+  page: {
+    paddingVertical: "5%",
+    flex: 1,
+    backgroundColor: "green"
+  },
+  view: {
+    flex: 1,
+    height: "30%",
+    backgroundColor: "red",
+    justifyContent: "flex-start",
+    alignItems: 'center',
+    borderColor: "white",
+  },
+  text: {
+    textAlign: "center",
+    backgroundColor: "#2f2b21",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    fontSize: 50,
+    color: "white",
+    borderRadius: 9,
+    overflow: "hidden"
+  }
+})
