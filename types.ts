fbe37bcbc516e9386ae4d6c9a4eac0fc2f6d7b1d
edthuman <edwardadirvine@aol.com>
2024-09-19 13:06:14 +0100

    add ModalVisible type declaration

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+export type ModalVisible = false | "sign up" | "log in"
+
 export interface HomeButtonsProps {
-    setModalVisible: React.Dispatch<React.SetStateAction<false | "sign up" | "log in">> 
+    setModalVisible: React.Dispatch<React.SetStateAction<ModalVisible>> 
 }
