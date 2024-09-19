48544103591b78b9a9a6ae6f432c2a5d986e61e3
edthuman <edwardadirvine@aol.com>
2024-09-19 13:29:54 +0100

    add HomeButton component

---
+++ app/HomeButton.tsx
@@ -0,0 +1,21 @@
+import { HomeButtonProps } from "@/types";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+export function HomeButton({ text, setModalVisible, style }: HomeButtonProps) {
+    return <Pressable style={[style, styles.button]} onPress={() => setModalVisible(text)}>
+    <Text style={styles.text}>{text}</Text>
+  </Pressable>
+}
+
+const styles = StyleSheet.create({
+    text: {
+        paddingVertical: 12,
+        textAlign: "center",
+        color: "white",
+        fontFamily: "Ubuntu_400Regular",
+        fontSize: 42,
+    },
+    button: {
+        borderRadius: 9
+    }
+})
