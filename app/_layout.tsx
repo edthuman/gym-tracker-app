c86638a6b5975520d687e9d2be57e9dd17067906
edthuman <edwardadirvine@aol.com>
2024-09-21 15:16:30 +0100

    add UserContext into _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Navigator from "./Navigator";
+import { useState } from "react";
+import UserContext from "./UserContext";
 
-export default function RootLayout() {  
+export default function RootLayout() {
+  const [user, setUser] = useState({
+    username: "guest",
+    _id: "guest"
+  });
+  const value = { user, setUser }
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Navigator/>
+      <UserContext.Provider value={value}>
+        <Navigator/>
+      </UserContext.Provider>
     </SafeAreaView>
   );
 }
