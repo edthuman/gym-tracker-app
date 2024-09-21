51863aa5c32f3c507b11077b09e7289ea34df39a
edthuman <edwardadirvine@aol.com>
2024-09-21 15:48:49 +0100

    add PageContext to _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,25 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Navigator from "./Navigator";
 import { useState } from "react";
 import UserContext from "../hooks/UserContext";
+import PageContext from "@/hooks/PageContext";
+import { CurrentPage } from "@/types";
 
 export default function RootLayout() {
     const [user, setUser] = useState({
         username: "guest",
         _id: "guest",
     });
-    const value = { user, setUser };
+    const userContextValues = { user, setUser };
+
+    const [page, setPage] = useState<CurrentPage>("Home")
+    const pageContextValues = {page, setPage}
 
     return (
         <SafeAreaView style={styles.safeArea}>
-            <UserContext.Provider value={value}>
-                <Navigator />
+            <UserContext.Provider value={userContextValues}>
+                <PageContext.Provider value={pageContextValues}>
+                    <Navigator />
+                </PageContext.Provider>
             </UserContext.Provider>
         </SafeAreaView>
     );
