a7c805f63b51b0f032fae75c7d6c430324d2e7d8
edthuman <edwardadirvine@aol.com>
2024-09-21 15:29:44 +0100

    move UserContext into hooks folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,28 @@
 import { StyleSheet } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 import Navigator from "./Navigator";
 import { useState } from "react";
-import UserContext from "./UserContext";
+import UserContext from "../hooks/UserContext";
 
 export default function RootLayout() {
-  const [user, setUser] = useState({
-    username: "guest",
-    _id: "guest"
-  });
-  const value = { user, setUser }
+    const [user, setUser] = useState({
+        username: "guest",
+        _id: "guest",
+    });
+    const value = { user, setUser };
 
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <UserContext.Provider value={value}>
-        <Navigator/>
-      </UserContext.Provider>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <UserContext.Provider value={value}>
+                <Navigator />
+            </UserContext.Provider>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: "green",
-  }
-})
+    safeArea: {
+        flex: 1,
+        backgroundColor: "green",
+    },
+});
