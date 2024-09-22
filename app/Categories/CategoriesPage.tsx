281546897c37adb059e6da43513d3f94adf67eca
edthuman <edwardadirvine@aol.com>
2024-09-22 18:05:03 +0100

    add back button to CategoriesPage

--- app/Categories/CategoriesPage.tsx
+++ app/Categories/CategoriesPage.tsx
@@ -1,12 +1,23 @@
-import { StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import CategoryButton from "./CategoryButton";
+import { useContext } from "react";
+import PageContext from "@/hooks/PageContext";
 
 export default function CategoriesPage() {
-    return <View style={styles.page}>
-        <CategoryButton category={"Weights"}/>
-        <CategoryButton category={"Cardio"}/>
-        <CategoryButton category={"Stretching"}/>
-    </View>
+    const {setPage} = useContext(PageContext)
+
+    return setPage ? (
+        <View style={styles.page}>
+            <Pressable onPress={() => setPage("WelcomePage")} style={styles.backButton}>
+                <Text>
+                    {"<--"} Go back
+                </Text>
+            </Pressable>
+            <CategoryButton category={"Weights"}/>
+            <CategoryButton category={"Cardio"}/>
+            <CategoryButton category={"Stretching"}/>
+        </View>
+    ) : null
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +26,13 @@ const styles = StyleSheet.create({
         backgroundColor: "#4a5458",
         justifyContent: "space-around",
         paddingHorizontal: "5%"
+    },
+    backButton: {
+        backgroundColor: "white",
+        marginVertical: "5%",
+        paddingVertical: "5%",
+        width: "30%",
+        justifyContent: "center",
+        alignItems: "center"
     }
 })
