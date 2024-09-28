50871801b793aae476f1192e546901e63ca67a12
edthuman <edwardadirvine@aol.com>
2024-09-28 17:53:34 +0100

    update exercisesArea to a ScrollView in ExercisesPage

--- app/Exercises/ExercisesPage.tsx
+++ app/Exercises/ExercisesPage.tsx
@@ -1,7 +1,7 @@
 import PageContext from "@/hooks/PageContext";
 import { Exercise } from "@/types";
 import { useContext } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import ExerciseButton from "./ExerciseButton";
 
 export default function ExercisesPage({ category }: { category: any }) {
@@ -36,7 +36,7 @@ export default function ExercisesPage({ category }: { category: any }) {
                 <Text>{"<-"} Go back</Text>
             </Pressable>
             <Text style={styles.title}>{categoryName} Exercises:</Text>
-            <View style={styles.exercisesArea}>
+            <ScrollView style={styles.exercisesArea}>
                 {category[categoryName].map((exercise: Exercise) => (
                     <ExerciseButton
                         exercise={exercise}
@@ -44,17 +44,16 @@ export default function ExercisesPage({ category }: { category: any }) {
                         icons={icons}
                     />
                 ))}
-            </View>
+            </ScrollView>
         </View>
     ) : null;
 }
 
 const styles = StyleSheet.create({
     page: {
-        flex: 1,
         backgroundColor: "blue",
         paddingHorizontal: "5%",
-        justifyContent: "space-around",
+        justifyContent: "space-around"
     },
     backButton: {
         backgroundColor: "white",
@@ -62,21 +61,17 @@ const styles = StyleSheet.create({
         paddingVertical: "5%",
         width: "30%",
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "center"
     },
     exercisesArea: {
         backgroundColor: "orange",
-        flex: 3,
         marginBottom: "5%",
-        flexWrap: "wrap",
-        flexDirection: "row",
-        alignContent: "space-around",
-        justifyContent: "space-around",
+        flexDirection: "column"
     },
     title: {
         backgroundColor: "cyan",
         textAlign: "center",
         paddingVertical: "3%",
-        fontSize: 20,
+        fontSize: 20
     },
 });
