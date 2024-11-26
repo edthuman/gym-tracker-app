9b6c34187c54926980d5e02c34d546e14ca1da75
edthuman <edwardadirvine@aol.com>
2024-11-26 15:57:59 +0000

    add handleDateInput event handler

--- app/SingleExercise/single-exercise-event-handlers.ts
+++ app/SingleExercise/single-exercise-event-handlers.ts
@@ -1,6 +1,9 @@
 import { LogFormData } from "@/types"
+import { DateTimePickerEvent } from "@react-native-community/datetimepicker"
 
-export function handleLogInput(input: string, setFormData: React.Dispatch<React.SetStateAction<LogFormData>>) {
+type SetFormData = React.Dispatch<React.SetStateAction<LogFormData>>
+
+export function handleLogInput(input: string, setFormData: SetFormData) {
     const isInputNan = Number.isNaN(Number(input))
     if (isInputNan) {
         return
@@ -9,6 +12,21 @@ export function handleLogInput(input: string, setFormData: React.Dispatch<React.
         return {
             ...currentData,
             log: input === "" ? "0" : input
+        }
+    })
+}
+
+export function handleDateInput(e: DateTimePickerEvent, date: Date | undefined, setFormData: SetFormData): void {
+    if (!date) {
+        return
+    }
+
+    const { nativeEvent: { timestamp } } = e;
+
+    setFormData(currData => {
+        return {
+            ...currData,
+            date: new Date(timestamp)
         }
     })
 }
