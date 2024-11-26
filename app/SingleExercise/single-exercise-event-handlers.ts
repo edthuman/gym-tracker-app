c20980b4241a21025c5d4f2cb91c8bc2b2d07acf
edthuman <edwardadirvine@aol.com>
2024-11-26 13:40:56 +0000

    add use of LogFormData to handleLogInput

--- app/SingleExercise/single-exercise-event-handlers.ts
+++ app/SingleExercise/single-exercise-event-handlers.ts
@@ -1,4 +1,6 @@
-export function handleLogInput(input: string, setFormData: React.Dispatch<React.SetStateAction<any>>) {
+import { LogFormData } from "@/types"
+
+export function handleLogInput(input: string, setFormData: React.Dispatch<React.SetStateAction<LogFormData>>) {
     const isInputNan = Number.isNaN(Number(input))
     if (isInputNan) {
         return
