d321c740a7df252029241c8c36f98c39b9232ae6
edthuman <edwardadirvine@aol.com>
2024-11-27 15:45:16 +0000

    add error handling and diary object formatting to handleLogSubmit

--- app/SingleExercise/single-exercise-event-handlers.ts
+++ app/SingleExercise/single-exercise-event-handlers.ts
@@ -1,9 +1,11 @@
 import { Diary, ExerciseWithCategory, LogFormData } from "@/types";
 import { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+import { getDateString } from "./single-exercise-utils";
 
 type SetFormData = React.Dispatch<React.SetStateAction<LogFormData>>
+type StringStateSetter = React.Dispatch<React.SetStateAction<string>>
 
-export function handleLogInput(input: string, setFormData: SetFormData, setLogInputError: React.Dispatch<React.SetStateAction<string>>): void {
+export function handleLogInput(input: string, setFormData: SetFormData, setLogInputError: StringStateSetter): void {
     const isInputNan = Number.isNaN(Number(input))
     if (isInputNan) {
         setLogInputError("Log must be a number")
@@ -38,16 +40,62 @@ export function handleDateInput(e: DateTimePickerEvent, date: Date | undefined, 
     })
 }
 
-export function handleLogSubmit(diary: Diary | undefined, exercise: ExerciseWithCategory, formData: LogFormData) {
-    // check exercise name
-    // check username
-    // check log amount is a number
+export function handleLogSubmit(username:string, diary: Diary | undefined, exercise: ExerciseWithCategory, formData: LogFormData, setError: StringStateSetter) {
+    setError("")
+    if (!exercise.name) {
+        setError("Something went wrong logging this exercise")
+        return
+    }
+    if (!username) {
+        setError("Could not update/add log with this account")
+        return
+    }
+    const {date, log } = formData
+    if (!log) {
+        setError("Log must have value")
+        return
+    }
+    const logIsNaN = Number.isNaN(Number(log))
+    if (logIsNaN) {
+        setError("Logged value must be a number")
+        return
+    }
+    if (Number(log) <= 0) {
+        setError("Logged value must be greater than 0")
+        return
+    }
+    if (Number(log) > 9999) {
+        setError("Logged value must be less than 10,000")
+        return
+    }
+    if (!date) {
+        setError("Invalid date given")
+        return
+    }
 
     const apiURL = process.env.EXPO_PUBLIC_API_URL
     if (!apiURL) {
-        // handle error
+        setError("Something went wrong, could not add log")
+        return
     }
+
     const diaryExists = diary !== undefined
+
+    const diaryToAdd: any = {}
+    if (diaryExists) {
+        diaryToAdd.logs = [{
+            log,
+            date: getDateString(date)
+        }]
+    } else {
+        diaryToAdd.username = username
+        diaryToAdd.exercise = exercise.name
+        diaryToAdd.logs = [{
+            log,
+            date: getDateString(date)
+        }]
+    }
+    
     if (diaryExists) {
         // patch request to /diaries/_id
     } else {
