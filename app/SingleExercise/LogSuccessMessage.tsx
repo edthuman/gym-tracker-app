ba961576c8aa974f477a8cdfed71a7a40194a3d9
edthuman <edwardadirvine@aol.com>
2024-11-28 16:30:10 +0000

    add buttons to LogSuccessMessage

--- app/SingleExercise/LogSuccessMessage.tsx
+++ app/SingleExercise/LogSuccessMessage.tsx
@@ -1,7 +1,15 @@
-import { StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export function LogSuccessMessage() {
-    return <Text style={styles.successMessage}>Log successfully added!</Text>
+    return <View>
+        <Text style={styles.successMessage}>Log successfully added!</Text>
+        <Pressable onPress={()=>console.log("log again pressed")}>
+            <Text>Log again</Text>
+        </Pressable>
+        <Pressable onPress={()=>console.log("close pressed")}>
+            <Text>Close</Text>
+        </Pressable>
+    </View>
 }
 
 const styles = StyleSheet.create({
