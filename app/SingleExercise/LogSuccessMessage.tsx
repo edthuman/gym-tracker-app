534e031f4281c1695d9a03aa3c5138d4201d2243
edthuman <edwardadirvine@aol.com>
2024-11-29 15:54:50 +0000

    add styling to LogSuccessMessage

--- app/SingleExercise/LogSuccessMessage.tsx
+++ app/SingleExercise/LogSuccessMessage.tsx
@@ -2,24 +2,45 @@ import { BooleanStateSetter } from "@/types";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export function LogSuccessMessage({ setIsLogging, setHasLogged }: { setIsLogging: BooleanStateSetter, setHasLogged: BooleanStateSetter }) {
-    return <View>
+    return <View style={styles.view}>
         <Text style={styles.successMessage}>Log successfully added!</Text>
-        <Pressable onPress={() => setHasLogged(false)}>
-            <Text>Log again</Text>
-        </Pressable>
-        <Pressable onPress={() => setIsLogging(false)}>
-            <Text>Close</Text>
-        </Pressable>
+        <View style={styles.buttonsArea}>
+            <Pressable onPress={() => setHasLogged(false)} style={styles.button}>
+                <Text>Log again</Text>
+            </Pressable>
+            <Pressable onPress={() => setIsLogging(false)} style={styles.button}>
+                <Text>Close</Text>
+            </Pressable>
+        </View>
     </View>
 }
 
 const styles = StyleSheet.create({
+    view: {
+        backgroundColor: "white",
+        height: "93%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center"
+    },
     successMessage: {
-        backgroundColor: "blue",
-        height: "90%",
         fontSize: 20,
         textAlign: "center",
         textAlignVertical: "center",
-        paddingVertical: "70%"
-    }
+        paddingVertical: 25
+    },
+    buttonsArea: {
+        marginTop: 10,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },
+    button: {
+        backgroundColor: "brown",
+        width: "35%",
+        height: 50,
+        alignItems: "center",
+        verticalAlign: "middle",
+        justifyContent: "center"
+    } 
 })
